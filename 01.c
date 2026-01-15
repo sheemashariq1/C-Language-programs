0fcefbcf99215a2ea64ae08573ca9dd02d52f362
sheemashariq1 <sheemashariq10@gmail.com>
2026-01-16 01:17:34 +0530

    Implement multiplication table program in C
    Add a program to print the multiplication table of a number.

--- 01.c
+++ 01.c
@@ -1,1 +1,13 @@
+//program to print the table of a number
+#include<stdio.h>
+void main()
+{
+  int n,i;
+  printf("Enter a number");
+  scanf("%d",&n);
+  for(i=1;i<=10;i++)
+{
+printf("%d X %d = %d",n,i,n*i);
+}
+}
 
